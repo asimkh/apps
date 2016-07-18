cae9a0cf659d02f87885fbf378ef97c6115ef7ee
Asim Kh <asimkh@hotmail.com>
2016-07-18 10:35:44 +0400

    v2

--- .env.local.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-            'DB_HOST', 'localhost',
-            'DB_DATABASE', 'hazdata',
-            'DB_USERNAME', 'homestead',
-            'DB_PASSWORD', 'secret'
-            
-        ];
-
--- resources/views/welcome.blade copy.php
+++
@@ -1,3 +0,0 @@
-extends('layouts.default')
-@section('conten')
-@stop
