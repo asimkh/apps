700c1163718a79af7d178631ddb7778c0e77a519
Asim Kh <asimkh@hotmail.com>
2016-07-18 10:56:20 +0400

    v2 update

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('connection.php');
-/*
-define('BASE_URL', 'http://mrmdev01/mccann-health-tips/v5-sample');
-define('CSS_PATH', 'http://mrmdev01/mccann-health-tips/v5-sample/css/');
-define('JS_PATH', 'http://mrmdev01/mccann-health-tips/v5-sample/js/');
-define('IMAGES_PATH', 'http://mrmdev01/mccann-health-tips/v5-sample/images/');
-*/
