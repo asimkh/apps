bd97c3882457a4f2842e9d107e96df3b20eed693
Asim Kh <asimkh@hotmail.com>
2016-07-18 14:49:04 +0400

    v2 update 1.6

--- config/database.php
+++ config/database.php
@@ -1,6 +1,14 @@
 <?php
 
+
+
 return [
+
+$url = parse_url(env("DATABASE_URL"));
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
 
     /*
     |--------------------------------------------------------------------------
@@ -67,12 +75,12 @@ return [
         ],
 
         'pgsql' => [
+           
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host'     => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
