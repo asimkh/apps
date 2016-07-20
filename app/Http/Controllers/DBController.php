5053a06011b725d3fde9ed00c79e5a8c5ac4233b
Asim Kh <asimkh@hotmail.com>
2016-07-20 15:03:25 +0400

    v2 update 1.8

---
+++ app/Http/Controllers/DBController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class DBController extends Controller
+{
+    //
+     public function db()
+    {
+    	return "Database connect";
+    }
+}
