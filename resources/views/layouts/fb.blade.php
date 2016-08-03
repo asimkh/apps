4d1a3de96e0ecb56d5bfab9265986953068f1925
Asim Kh <asimkh@hotmail.com>
2016-08-03 18:29:08 +0400

    v4.2

--- resources/views/layouts/fb.blade.php
+++ resources/views/layouts/fb.blade.php
@@ -6,8 +6,8 @@
 
     <title>حـاضــر | HAZZIR </title>
     <link rel="stylesheet" type="text/css" href="{{ elixir('css/all.css')}}">
-    <link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
     
 
     <!-- fb -->
@@ -34,7 +34,7 @@
     </div>
     
    <script src="{{ asset('/js/all.js') }}"></script>
-   
+
      <!--@include('layouts.footer')-->
       
    
